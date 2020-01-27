973d2066e0fbb0e02002aa2a8c768f6dc313b5ec
Dardan Iljazi <sleepersdardan@gmail.com>
2020-01-27 23:06:50 +0100

    Typo

--- Assets/AR/ImageRecognitionExample.cs
+++ Assets/AR/ImageRecognitionExample.cs
@@ -30,7 +30,7 @@ public class ImageRecognitionExample : MonoBehaviour
 
     public void SelectQuizzInIndex(object imageNameAsAQuizzId)
     {
-        // Here we create a new Quizz and define its id
+        // Here we create a new Quizz and define its id (
         Quizz quizzToShow = new Quizz();
         quizzToShow.SetQuizzId(imageNameAsAQuizzId);
 
